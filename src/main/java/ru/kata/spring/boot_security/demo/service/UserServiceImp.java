8106848af0cf8eb245e14ee7036d6dd13ab71b9d
adel-galyameev <112763316+adel-galyameev@users.noreply.github.com>
2023-01-03 14:57:57 +0300

    v1.0

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,55 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDAO;
+import ru.kata.spring.boot_security.demo.model.User;
+
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImp implements UserService{
+
+
+    private UserDAO userDAO;
+    @Autowired
+    public UserServiceImp (UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDAO.listUsers();
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        userDAO.save(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getUser(int id) {
+        return userDAO.getUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userDAO.delete(id);
+    }
+
+
+}
